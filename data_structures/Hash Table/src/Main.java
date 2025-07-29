edf07cb2c898baf9cddc0972e2923c99959e6082
PravinLengare <lengarepravin115@gmail.com>
2025-07-30 00:48:12 +0530

    hash table

--- data_structures/Hash Table/src/Main.java
+++ data_structures/Hash Table/src/Main.java
@@ -1,6 +1,22 @@
+import hash_table.com.HashTable;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        HashTable newHashTable = new HashTable();
+        newHashTable.set("p",3);
+        newHashTable.set("q",4);
+        newHashTable.set("r",5);
+        newHashTable.set("s",6);
+        newHashTable.set("t",7);
+
+        newHashTable.printTable();
+
+
+        System.out.println("The get value of p key is : "+newHashTable.get("p"));
+        System.out.println("The get value of p key is : "+newHashTable.get("a"));
+
+        System.out.println(newHashTable.keys());
+
     }
 }
