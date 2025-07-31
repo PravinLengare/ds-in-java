8ca45c04178398e008ee492dfbf833f2a14bffd8
PravinLengare <lengarepravin115@gmail.com>
2025-08-01 00:16:15 +0530

    string adv

---
+++ data_structures/Recursion/src/recursion/com/String/SubSetSum.java
@@ -0,0 +1,18 @@
+package recursion.com.String;
+
+public class SubSetSum {
+    static void subsetSum(int arr[],int n,int idx,int ans){
+        if (idx >= n){
+            System.out.println(ans);
+            return;
+        }
+        subsetSum(arr,n,idx+1,ans + arr[idx]); // add
+
+        subsetSum(arr,n,idx+1,ans); // don't add
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,4,5};
+        subsetSum(arr,3,0,0);
+    }
+}
---
+++ data_structures/Recursion/src/recursion/com/String/Subsequence.java
@@ -0,0 +1,42 @@
+package recursion.com.String;
+
+import java.util.ArrayList;
+
+public class Subsequence {
+    static ArrayList<String> subSeq(String s){
+        ArrayList<String> ans = new ArrayList<>();
+        // Base case
+        if(s.length() == 0){
+            ans.add(" ");
+            return ans;
+        }
+        char curr = s.charAt(0);
+        // Recursive Work
+        ArrayList<String> small = subSeq(s.substring(1));
+        // Self Work
+        for (String ss:small) {
+            ans.add(ss);
+            ans.add(curr + ss);
+        }
+        return ans;
+    }
+    static void printSubSeq(String s,String currAns){
+
+        if (s.length() == 0){
+            System.out.println(currAns);
+            return;
+        }
+        char curr = s.charAt(0);
+        String remString =  s.substring(1);
+        printSubSeq(remString,currAns+curr);
+        printSubSeq(remString,currAns);
+    }
+    public static void main(String[] args) {
+        ArrayList<String> ans = subSeq("abc");
+        for (String ss: ans){
+            System.out.println(ss);
+        }
+        System.out.println("Using without extra space");
+        printSubSeq("abc","");
+    }
+}
